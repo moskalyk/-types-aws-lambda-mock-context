49407101f0d39090fdc384599e0b282c2deb8c4a
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2017-03-28 13:26:51 -0400

    added package.json with lint and test scripts

--- aws-lambda-mock-context-tests.ts
+++ aws-lambda-mock-context-tests.ts
@@ -1,12 +1,12 @@
-import context = require('aws-lambda-mock-context');
-import * as Alexa from 'alexa-sdk';
+import context = require("aws-lambda-mock-context");
+import * as Alexa from "alexa-sdk";
 
 const launchRequestJson: any = {
   request: {
     locale: "en-US",
     requestId: "amzn1.echo-api.request.[unique-value-here]",
     timestamp: "2016-10-27T18:21:44Z",
-    type: "LaunchRequest"
+    type: "LaunchRequest",
   },
   session: {
     application: {applicationId: "amzn1.ask.skill.[unique-value-here]"},
@@ -15,7 +15,7 @@ const launchRequestJson: any = {
     sessionId: "amzn1.echo-api.session.[unique-value-here]",
     user: {userId: "amzn1.ask.account.[unique-value-here]"},
   },
-  version: "1.0"
+  version: "1.0",
 };
 
 const ctx = context();
